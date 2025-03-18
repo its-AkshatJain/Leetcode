a318ae3556aa1061c479d10e16cac0634f3877f8
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-03-18 13:15:07 +0530

    Time: 0 ms (100%), Space: 60.9 MB (64.15%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+
+        // Sliding window
+        int max_window_size = 0;
+        int curr_sum = 0;
+        int xor_sum = 0;
+        while(right<n){
+            curr_sum += nums[right];
+            xor_sum ^= nums[right];
+
+            while(xor_sum!=curr_sum){
+                curr_sum -= nums[left];
+                xor_sum ^= nums[left];
+                left++;
+            }
+
+            max_window_size = max(max_window_size, right - left + 1);
+            right++;
+        }
+        return max_window_size;
+    }
+};
