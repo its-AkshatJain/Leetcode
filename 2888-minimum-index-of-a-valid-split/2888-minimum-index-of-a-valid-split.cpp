eeb366890db387ae8471dc58c5d100fee2b4da90
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-03-28 02:26:50 +0530

    Time: 4 ms (78.84%), Space: 90 MB (71.78%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int mooreVotingAlgo(vector<int>& nums){
+        int majority_element = nums[0];
+        int freq = 1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != majority_element)  freq--;
+            else                             freq++;
+            if(freq == 0){
+                majority_element = nums[i];
+                freq = 1;
+            }
+        }
+        return majority_element;
+    }
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        int majority_element = mooreVotingAlgo(nums);
+
+        int max_freq = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == majority_element) max_freq++;
+        }
+
+        int prefix_count = 0;
+        for(int i=0; i<n-1; i++){
+            if(nums[i] == majority_element){
+                prefix_count++;
+                max_freq--;
+            }
+            if(prefix_count > (i+1)/2 && max_freq > (n-i-1)/2){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
