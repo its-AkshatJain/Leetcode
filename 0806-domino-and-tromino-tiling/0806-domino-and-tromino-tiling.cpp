a72fe886bbc42cdea3e471ba4095ea90cb36fd12
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-05-07 02:30:13 +0530

    Time: 4 ms (8.64%), Space: 12.2 MB (6.25%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,17 +1,31 @@
 class Solution {
+    int dp[1005][1005];
     int MOD = 1e9 + 7;
+    int numTilingWays(int r1, int r2){
+        if(r1 == 0 && r2 == 0){
+            return 1; //valid tiling
+        }
+        if(r1<=0 || r2<=0) return 0; //invalid
+        if(dp[r1][r2] != -1) return dp[r1][r2];
+
+        long long count = 0;
+        if(r1 == r2){
+            count+= numTilingWays(r1-2, r2-2); //H
+            count+= numTilingWays(r1-1, r2-1); //V             
+            count+= numTilingWays(r1-2, r2-1); //inverted mirrored L
+            count+= numTilingWays(r1-1, r2-2); // mirrored L
+        }else if(r1>r2){
+            count+= numTilingWays(r1-2, r2); //H row1
+            count+= numTilingWays(r1-2, r2-1); // mirrored L
+        }else{
+            count+= numTilingWays(r1, r2-2); //H row2
+            count+= numTilingWays(r1-1, r2-2); // mirrored L
+        }
+        return dp[r1][r2] = count%MOD;
+    }
 public:
     int numTilings(int n) {
-       if(n<3) return n;
-       vector<int> dp(n+1);
-
-       dp[0] = 1;
-       dp[1] = 1;
-       dp[2] = 2;
-
-       for(int i=3; i<=n; i++){
-         dp[i] = (1LL*2*dp[i-1] + dp[i-3])%MOD; 
-       }
-       return dp[n]; 
+       memset(dp, -1, sizeof(dp));
+       return numTilingWays(n,n); 
     }
 };
