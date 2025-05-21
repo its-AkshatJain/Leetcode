069de82f051dbc58ec33eb744a613e1ffb79d514
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-05-22 01:58:28 +0530

    Time: 0 ms (100%), Space: 18.6 MB (52.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,23 +4,48 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<int> row(m,false);
-        vector<int> col(n,false);
+        int firstRowImpacted = false; 
+        int firstColImpacted = false; 
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j]==0){
-                    row[i] = true;
-                    col[j] = true;
-                }
+        for(int i=0; i<n; i++){
+            if(matrix[0][i] == 0){
+                firstRowImpacted = true;
             }
         }
 
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(row[i] == true || col[j] == true){
+            if(matrix[i][0] == 0){
+                firstColImpacted = true;
+            }
+        }
+
+        
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if(firstRowImpacted){
+            for(int i=0; i<n; i++){
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(firstColImpacted){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
     }
