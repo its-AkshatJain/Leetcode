62915a10f970f96fb759fd80f2cfc83dec19823c
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-05-22 17:38:14 +0530

    Time: 131 ms (39.52%), Space: 224.5 MB (40.72%) - LeetHub

---
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        priority_queue<int> available_query; //maxheap
+        priority_queue<int, vector<int>, greater<int> > used_query; //minheap
+        sort(queries.begin(), queries.end());
+
+        int query_pos = 0;
+        int applied_count = 0;
+
+        for(int i=0; i<n; i++){
+            while(query_pos<queries.size() && queries[query_pos][0] == i){
+                available_query.push(queries[query_pos][1]);
+                query_pos++;
+            }
+
+            // Previously overlapping query must be adjusted
+            nums[i] -= used_query.size();
+
+            while(nums[i]>0 && !available_query.empty() && available_query.top()>= i){
+                used_query.push(available_query.top());
+                available_query.pop();
+                nums[i]--;
+                applied_count++;
+            }
+
+            if(nums[i]>0) return -1;
+
+            while(!used_query.empty() && used_query.top()==i){
+                used_query.pop();
+            }
+        }
+        return queries.size() - applied_count;
+    }
+};
