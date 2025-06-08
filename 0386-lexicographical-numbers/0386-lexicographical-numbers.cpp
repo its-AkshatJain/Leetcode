a3372e127bdec1f703b87e028ae373b31b61e16d
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-06-08 16:40:47 +0530

    Time: 3 ms (45.99%), Space: 14.3 MB (44.11%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    void dfs(int curr, int& n, vector<int> &ans){
+        if(curr>n) return;
+
+        ans.push_back(curr);
+        for(int i=0; i<=9; i++){  //Not most significant bit
+            dfs(curr*10 + i, n, ans);
+        }
+    }
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for(int i=1; i<=9; i++){
+            dfs(i, n, ans);
+        }
+        return ans;
+    }
+};
