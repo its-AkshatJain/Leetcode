9ee6b7ee50fa08e7d13cb9078b00e400b0ed2478
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-06-11 02:45:06 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.6%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int sum = 0;
+        //Kadane's Algo
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
+
+            if(sum > maxi) maxi = sum;
+
+            if(sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+};
