036797d3169efbcd155bd5f1eb0506be15a4921f
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-06-19 17:42:42 +0530

    Time: 39 ms (32.78%), Space: 87 MB (39.38%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.cpp
@@ -4,15 +4,11 @@ public:
         sort(nums.begin(), nums.end());
         
         int minSubsequence = 1;
-        int maxi = nums[0]; 
-        int mini = nums[0];
+        int start = nums[0]; 
         for(int i=1; i<nums.size(); i++){
-            maxi = max(maxi, nums[i]); 
-            mini = min(mini, nums[i]);
-
-            if(maxi - mini > k){
+            if(nums[i] - start > k){
                 minSubsequence++;
-                maxi = mini = nums[i];
+                start = nums[i];
             }
         }
         return minSubsequence;
