921772c16c8c2571ec136170691b37299602cbef
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2025-07-26 19:28:26 +0530

    Time: 154 ms (74.58%), Space: 324.3 MB (28.81%) - LeetHub

---
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
+        long long valid = 0;       
+        vector<vector<int>> conflictingPoints(n+1);
+
+        for(auto &p : conflictingPairs) { 
+            //(a, b)
+            int a = min(p[0], p[1]);
+            int b = max(p[0], p[1]);
+
+            conflictingPoints[b].push_back(a); //{1,2,a,,,,b}
+        }
+
+        int maxConflict = 0;
+        int secondMaxConflict = 0;
+
+        vector<long long> extra(n+1, 0);
+
+        for(int end = 1; end <= n; end++) {
+
+            for(int &u : conflictingPoints[end]) { //check all conflicting points of end
+                if(u >= maxConflict) {
+                    secondMaxConflict = maxConflict;
+                    maxConflict = u;
+                } else if(u > secondMaxConflict) {
+                    secondMaxConflict = u;
+                }
+            }
+
+            valid += end - maxConflict;
+            extra[maxConflict] += maxConflict - secondMaxConflict;
+        }
+
+        return valid + *max_element(extra.begin(), extra.end());
+    }
+};
