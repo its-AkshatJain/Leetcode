c7f0837b7ff5f85edec15ed14dd46a417592391c
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-02-22 14:03:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.75%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int binaryGap(int n) {
-        int curr = 0;
         int prev = -1;
-
         int result = 0;
 
-        while(n>0){
-            if((n&1) > 0){
+        for(int curr = 0; curr<32; curr++){
+            if(((n>>curr) & 1) > 0){
                 result = (prev != -1) ? max(result, curr-prev) : result;
                 prev = curr;
             }
-            curr++;
-            n = n>>1;
         }
         return result;
     }
