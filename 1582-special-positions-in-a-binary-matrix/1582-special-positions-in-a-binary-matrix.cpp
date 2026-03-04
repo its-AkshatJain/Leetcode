9af7eac6aaa725f6e2331a9a42ecb440d4a7d795
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-03-04 18:45:51 +0530

    Time: 4 ms (10.73%), Space: 17.2 MB (20.57%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -4,27 +4,23 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         int result = 0;
+        vector<int> rowCount(m, 0);
+        vector<int> colCount(n, 0);
+
+        for(int row=0; row<m; row++){
+            for(int col=0; col<n; col++){
+                if(mat[row][col] == 1){
+                    rowCount[row]++;
+                    colCount[col]++;
+                }
+            }
+        }
 
         for(int row=0; row<m; row++){
             for(int col=0; col<n; col++){
                 if(mat[row][col] == 0) continue;
 
-                bool special = true;
-                // check columns
-                for(int r=0; r<m; r++){
-                    if(r!=row && mat[r][col] == 1){
-                        special = false;
-                    }
-                }
-
-                // check rows
-                for(int c=0; c<n; c++){
-                    if(c!=col && mat[row][c] == 1){
-                        special = false;
-                    }
-                }
-
-                if(special == true){
+                if(rowCount[row] == 1 && colCount[col] == 1){
                     result++;
                 }
             }
