1da7b532c084fa60073f8777bc9531c38429846c
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-03-29 20:50:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (25.16%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        int condition1 = (s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0]);
+        int even[26] = {0};
+        int odd[26] = {0};
 
-        int condition2 = (s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1]);
+        for(int i=0; i<4; i++){
+            if(i%2 == 0){
+                even[s1[i]-'a']++;
+                even[s2[i]-'a']--;
+            }else{
+                odd[s1[i]-'a']++;
+                odd[s2[i]-'a']--;
+            }
+        }
 
-        return condition1 && condition2;
+        for(int i=0; i<26; i++){
+            if(even[i] !=0 || odd[i]!=0){
+                return false;
+            }
+        }
+
+        return true;
     }
 };
