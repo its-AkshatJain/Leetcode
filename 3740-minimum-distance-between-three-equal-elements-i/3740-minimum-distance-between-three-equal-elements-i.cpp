a909241abdcc777b65a193e3dba02b0e28f0cad4
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-04-10 16:48:56 +0530

    Time: 2 ms (75.63%), Space: 29.8 MB (31.72%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -3,16 +3,22 @@ public:
     int minimumDistance(vector<int>& nums) {
         int n = nums.size();
         int minDist = INT_MAX;
-
-        if(n < 3) return -1;
+        unordered_map<int, vector<int>> mp;
 
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if(nums[i] == nums[j] && nums[j] == nums[k]){
-                        minDist = min(minDist, abs(i - j) + abs(j - k) + abs(k - i));
-                    }
-                }
+            mp[nums[i]].push_back(i);
+        }
+        
+        for(auto &p: mp){
+            auto &v = p.second;
+            int m = v.size();
+
+            if(m < 3) continue;
+
+            for(int i=0; i<=m-3; i++){
+                int k = i+2;
+                int dist = 2*(v[k]-v[i]);
+                minDist = min(minDist, dist);
             }
         }
 
