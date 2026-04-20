53bb0cbfddad1187fbb077f27b1cb34ff1c52772
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-04-20 17:07:55 +0530

    Time: 0 ms (100%), Space: 11.9 MB (99.83%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -4,15 +4,15 @@ public:
         int n = colors.size();
         int result = 0;
 
-        int i=0;
-        while(colors[i] == colors[n-1]) i++;
-        
-        result = max(result, abs(i - (n-1)) );
+        for(int i=0; i<n; i++){
+            if(colors[i]  != colors[0]){
+                result = max(result, i);
+            }
 
-        i = n-1;
-        while(colors[0] == colors[i]) i--;
-        
-        result = max(result, i);
+            if(colors[i]  != colors[n-1]){
+                result = max(result, abs(i - (n-1)) );
+            }
+        }
 
         return result;
     }
