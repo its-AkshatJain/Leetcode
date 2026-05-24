4a72ee27306faccb48bd9a8d628c9ae269136536
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-05-24 16:59:51 +0530

    Time: 7 ms (94.19%), Space: 20.7 MB (21.56%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
-    int n;
-    vector<int> t;
-    int solve(vector<int>& arr, int i, int d){
-        if(t[i] != -1){
-            return t[i];
-        }
-        
-        int result = 1;
-
-        // Left
-        for(int j = i-1; j>=(max(0, i-d)); j--){
-            if(arr[j] >= arr[i]) break;
-            result = max(result, 1+solve(arr, j, d));
-        }
-
-        // Right
-        for(int j = i+1; j<=min(n-1, i+d); j++){
-            if(arr[j] >= arr[i]) break;
-            result = max(result, 1+solve(arr, j, d));
-        }
-
-        return t[i] = result;
-    }
-
     int maxJumps(vector<int>& arr, int d) {
-        n = arr.size();
-        int result = 1;
-        t.resize(n+1, -1);
+        int n = arr.size();
+        vector<int> t(n+1, -1);
 
+        vector<pair<int, int>> vec;
         for(int i=0; i<n; i++){
-            result = max(result, solve(arr, i, d));
+            vec.push_back({arr[i], i});
         }
 
-        return result;
+        sort(vec.begin(), vec.end());
+        
+        for(auto &it: vec){
+            int val = it.first;
+            int i = it.second;
+            int result = 1;
+
+            for(int j = i-1; j>=max(0, i-d); j--){
+                if(arr[j] >= arr[i]) break;
+                result = max(result, 1+t[j]);
+            }
+
+            // Right
+            for(int j = i+1; j<=min(n-1, i+d); j++){
+                if(arr[j] >= arr[i]) break;
+                result = max(result, 1+t[j]);
+            }
+
+            t[i] = result;
+        }
+
+        return *max_element(t.begin(), t.end());
     }
 };
