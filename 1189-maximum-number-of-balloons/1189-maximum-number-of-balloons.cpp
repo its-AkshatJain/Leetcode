555453c545026b4540b39725618da9561ea73d53
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-06-22 16:22:46 +0530

    Time: 0 ms (100%), Space: 8.8 MB (95.38%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -7,15 +7,13 @@ public:
             freq[ch - 'a']++;
         }
 
-        while (freq['b' - 'a'] > 0 && freq['a' - 'a'] > 0 && freq['l' - 'a'] > 1 &&
-               freq['o' - 'a'] > 1 && freq['n' - 'a'] > 0) {
-            ans++;
-            freq['b' - 'a']--;
-            freq['a' - 'a']--;
-            freq['l' - 'a'] -= 2;
-            freq['o' - 'a'] -= 2;
-            freq['n' - 'a']--;
-        }
+        ans = min({
+            freq['b' - 'a'],
+            freq['a' - 'a'],
+            freq['l' - 'a'] / 2,
+            freq['o' - 'a'] / 2,
+            freq['n' - 'a'],
+        });
 
         return ans;
     }
