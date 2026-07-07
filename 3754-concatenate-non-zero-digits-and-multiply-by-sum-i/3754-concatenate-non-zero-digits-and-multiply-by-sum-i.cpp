aef54c5b92ff2c98bb40bce1361158dbe632ce04
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-07-07 18:28:41 +0530

    Time: 0 ms (100%), Space: 8.9 MB (89.09%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     long long sumAndMultiply(int n) {
         int sum = 0;
-        int revNum = 0, num = 0 ;
-        
-        while(n){
-            int digit = n%10;
-            if(digit != 0) revNum = revNum*10 + digit;
-            n /= 10; 
+        int num = 0;
+        long long place = 1;
+
+        while (n) {
+            int digit = n % 10;
+            if (digit != 0) {
+                num = digit * place + num;
+                place *= 10;
+                sum += digit;
+            }
+            n /= 10;
         }
 
-        while(revNum){
-            int digit = revNum%10;
-            sum += digit;
-            num = num*10 + digit;
-            revNum /= 10; 
-        }
-
-        return 1LL*num*sum;
+        return 1LL * num * sum;
     }
 };
