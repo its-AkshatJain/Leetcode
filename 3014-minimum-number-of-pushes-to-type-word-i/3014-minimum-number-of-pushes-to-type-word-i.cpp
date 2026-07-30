9e16c555af6e2c94c8ce775c9a9cee3f1d0672ed
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-07-30 16:24:36 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,14 +4,8 @@ public:
         int n = word.length();
         int ans = 0;
 
-        if(n <= 8){
-            ans = n;
-        }else if(n <= 16){
-            ans = 8 + ((n-8)*2);
-        }else if(n <= 24){
-            ans = 8 + 16 + ((n-16)*3);
-        }else{
-            ans = 8 + 16 + 24 + ((n-24)*4);
+        for (int i = 0; i < n; i++) {
+            ans += (i / 8) + 1;
         }
 
         return ans;
