11e2c412da978ac2422eed3b40245e80aeb215a0
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-08-01 18:55:01 +0530

    Time: 0 ms (100%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -14,23 +14,16 @@ public:
             return t[i][j];
         }
 
-        int take_i = nums[i] + min(solve(i+2, j, nums), solve(i+1, j-1, nums));
-        int take_j = nums[j] + min(solve(i, j-2, nums), solve(i+1, j-1, nums));
+        int take_i = nums[i] - solve(i+1, j, nums);
+        int take_j = nums[j] - solve(i, j-1, nums);
 
         return t[i][j] = max(take_i, take_j);
     }
-    
+
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
         memset(t, -1, sizeof(t));
 
-        int total_score = 0;
-        for(int i=0; i<n; i++){
-            total_score += nums[i];
-        }
-        int p1_score = solve(0, n-1, nums);
-        int p2_score = total_score - p1_score;
-
-        return p1_score >= p2_score;
+        return solve(0, n-1, nums)>=0;
     }
 };
