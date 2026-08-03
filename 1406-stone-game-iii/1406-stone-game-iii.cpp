038b33964f3ae057ebdd060b80555974950b8fec
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-08-04 02:12:46 +0530

    Time: 3 ms (98.94%), Space: 136.1 MB (75.1%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int n;
-    int solve(int i, vector<int>& t, vector<int>& stoneValue) {
-        if (i >= n) {
-            return 0;
-        }
-
-        if (t[i] != -1) {
-            return t[i];
-        }
-
-        int result = stoneValue[i] - solve(i + 1, t, stoneValue);
-
-        if (i + 1 < n) {
-            result = max(result, stoneValue[i] + stoneValue[i + 1] -
-                                     solve(i + 2, t, stoneValue));
-        }
-        if (i + 2 < n) {
-            result = max(result, stoneValue[i] + stoneValue[i + 1] +
-                                     stoneValue[i + 2] -
-                                     solve(i + 3, t, stoneValue));
-        }
-        return t[i] = result;
-    }
-
     string stoneGameIII(vector<int>& stoneValue) {
-        n = stoneValue.size();
-        vector<int> t(n + 1, -1);
+        int n = stoneValue.size();
+        vector<int> t(n + 1);
 
-        int diff = solve(0, t, stoneValue);
+        for (int i = n - 1; i >= 0; i--) {
+            t[i] = stoneValue[i] - t[i + 1];
+
+            if (i + 2 <= n) {
+                t[i] = max(t[i], stoneValue[i] + stoneValue[i + 1] - t[i + 2]);
+            }
+            if (i + 3 <= n) {
+                t[i] = max(t[i], stoneValue[i] + stoneValue[i + 1] +
+                                       stoneValue[i + 2] - t[i + 3]);
+            }
+        }
+
+        int diff = t[0];
 
         if (diff > 0) {
             return "Alice";
