cea59e0b8909975487c919e5d3d8af0fba62b93b
Akshat Jain <129594034+its-AkshatJain@users.noreply.github.com>
2026-08-12 17:30:34 +0530

    Time: 55 ms (90.73%), Space: 149.4 MB (27.87%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,16 +6,23 @@ public:
         int n = nums.size();
         unordered_map<int,int> mp;
         int result=0;
+        int culprit = 0;
 
         while(j<n){
             mp[nums[j]]++;
-
-            while(i<j && mp[nums[j]] > k){
-                mp[nums[i]]--;
-                i++;
+            if(mp[nums[j]] == k+1){
+                culprit++;
             }
 
-            result = max(result, j-i+1);
+           if(culprit > 0){
+                mp[nums[i]]--;
+                if(mp[nums[i]] == k){
+                    culprit--;
+                }
+                i++;
+           }
+
+            if(culprit == 0) result = max(result, j-i+1);
             j++;
         }
         
